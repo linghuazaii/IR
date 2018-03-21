4f7b3f410a8416ff17abae3e07ffbfa871a7d565
Ubuntu <ubuntu@ip-172-31-24-26.ap-northeast-1.compute.internal>
2018-03-21 14:34:54 +0000

    m

--- intro_to_information_retrieval/boolean_retrieval/boolean_retrieval.py
+++ intro_to_information_retrieval/boolean_retrieval/boolean_retrieval.py
@@ -2,3 +2,28 @@
 # -*- coding: utf-8 -*-
 # This file is auto-generated.Edit it at your own peril.
 
+news_dir = '../news'
+max_content_length = 64 * 1024 # 64k
+delimiters = [' ', ',', '.', '\n']
+
+test = '../news/1.news'
+
+def tokenize(data, delimeter):
+
+
+def read_news(file_name):
+    title = None
+    content = None
+
+    with open(file_name) as f:
+        title = f.readline()
+        content = f.read(max_content_length)
+    f.close()
+
+    return title[:-1], content[:-1]
+
+
+if __name__ == '__main__':
+    title, content = read_news(test)
+    print title
+    print content
