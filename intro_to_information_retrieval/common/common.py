9ad096d7697c6c472214b91975802ae26f20d4fd
Ubuntu <ubuntu@ip-172-31-24-26.ap-northeast-1.compute.internal>
2018-03-21 14:36:22 +0000

    m

--- intro_to_information_retrieval/common/common.py
+++ intro_to_information_retrieval/common/common.py
@@ -2,3 +2,15 @@
 # -*- coding: utf-8 -*-
 # This file is auto-generated.Edit it at your own peril.
 
+def BKDRHash(key, itype):
+    seed = 1313131313
+    hash_val = 0
+    for c in key:
+        hash_val = (hash_val * seed) + ord(c)
+
+    return ((hash_val & 0x7FFFFFFFFFFFFFF) | itype << 60)
+
+'''
+if __name__ == '__main__':
+    print BKDRHash('the vampire never exists!', 0)
+'''
